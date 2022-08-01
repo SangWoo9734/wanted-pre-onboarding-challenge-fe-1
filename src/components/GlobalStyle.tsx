e1812eb79f87949d291a25f4372bb04b2ccbb8d2
Parksangwoo <psu8430@gmail.com>
2022-08-01 19:29:08 +0900

    Feat: 전역 스타일 지정

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *{margin:0;padding:0;color:inherit;}
+  * ::-webkit-scrollbar {display: none;}
+  *,:after, :before {box-sizing:border-box;flex-shrink:0;}
+  :root {-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;text-size-adjust:100%;cursor:default;line-height:1.5;overflow-wrap:break-word;-moz-tab-size:4;tab-size:4}
+  html {width: 100%; height:100%;}
+  body { max-width: 800px; height: 100%; margin: 0 auto; color: black; background: white; box-shadow: 0 0 20px 0 #d7d7d7}
+  img {display: block;max-width:100%;};
+  li {list-style: none};
+  p {margin: 0};
+  button {background:none;border:0;cursor:pointer;}
+  a {text-decoration:none}
+`;
+
+export default GlobalStyle;
