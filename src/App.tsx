a2a163afa1f7ead87eaf48bf30bdc8342a329788
Parksangwoo <psu8430@gmail.com>
2022-08-01 19:29:50 +0900

    Feat: Router 분기 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+import GlobalStyle from "./components/GlobalStyle";
+import Main from "./pages/Main";
+import Auth from "./pages/Auth";
+import Todo from "./pages/Todo";
+import TodoDetail from "./pages/TodoDetail";
+
+const ContentWrapper = styled.div`
+  position: relative;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <ContentWrapper>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/todo" element={<TodoDetail />} />
+      </Routes>
+    </ContentWrapper>
   );
 }
 
