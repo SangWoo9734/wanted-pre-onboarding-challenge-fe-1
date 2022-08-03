ff20c8ce7289a44d8e15d5fd7962800da3e8c696
Parksangwoo <psu8430@gmail.com>
2022-08-04 01:09:30 +0900

    Design: 레이아웃 최대 넓이 조정

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   *,:after, :before {box-sizing:border-box;flex-shrink:0;}
   :root {-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;text-size-adjust:100%;cursor:default;line-height:1.5;overflow-wrap:break-word;-moz-tab-size:4;tab-size:4}
   html {width: 100%; height:100%;}
-  body { max-width: 800px; height: 100%; margin: 0 auto; color: black; background: white; box-shadow: 0 0 20px 0 #d7d7d7}
+  body { max-width: 400px; height: 100%; margin: 0 auto; color: black; background: white; box-shadow: 0 0 20px 0 #d7d7d7}
   img {display: block;max-width:100%;};
   li {list-style: none};
   p {margin: 0};
