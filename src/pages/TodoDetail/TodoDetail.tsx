558b1b2320e4be24138635dab5f6dfbd4477d61a
Parksangwoo <psu8430@gmail.com>
2022-08-05 03:27:17 +0900

    Feat: Todo Detail 조회 / 수정 / 삭제 기능 구현

--- src/pages/TodoDetail/TodoDetail.tsx
+++ src/pages/TodoDetail/TodoDetail.tsx
@@ -1,24 +1,27 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import * as S from "./style";
+import { getTodoById, TodoType, deleteTodo } from "../../api/todoApi";
 import Button from "../../components/Button";
-
-interface Todo {
-  title: string;
-  content: string;
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-}
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function TodoDetail() {
-  const [todoDetail, setTodoDetail] = useState<Todo>({
-    title: "hi",
-    content: "hello",
-    id: "z3FGrcRL55qDCFnP4KRtn",
-    createdAt: "2022-07-24T14:15:55.537Z",
-    updatedAt: "2022-07-25T14:15:55.537Z",
-  });
+  const [todoDetail, setTodoDetail] = useState<TodoType>();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      void getTodoById(id).then((response) => {
+        if (response) setTodoDetail(response);
+        else {
+          navigate("/todo");
+        }
+      });
+    } else {
+      navigate("/todo");
+    }
+  }, []);
 
   const dateFormating = useCallback((date: string) => {
     const [first, last] = date.split("T");
@@ -26,28 +29,41 @@ function TodoDetail() {
     return `${first} ${last.slice(0, last.length - 5)}`;
   }, []);
 
+  const deleteAction = (id: string) => {
+    void deleteTodo(id).then(() => navigate("/todo"));
+  };
+
   return (
     <S.ComponentWrapper>
-      <S.TodoDetailHeader>
-        <input type="checkbox" />
-        <p>{todoDetail.title}</p>
-      </S.TodoDetailHeader>
-      <S.TodoDetailDate>
-        <p>
-          <span>Created At:</span> {dateFormating(todoDetail.createdAt)}
-        </p>
-        <p>
-          <span>Updated At:</span> {dateFormating(todoDetail.updatedAt)}
-        </p>
-      </S.TodoDetailDate>
-      <S.TodoDetailContent>
-        <p className="content-title">Content</p>
-        <p className="content-body">{todoDetail.content}</p>
-      </S.TodoDetailContent>
-      <S.TodoDetailButtons>
-        <Button content="Edit" />
-        <Button content="Delete" />
-      </S.TodoDetailButtons>
+      {todoDetail && (
+        <>
+          <S.TodoDetailHeader>
+            <div>
+              <input type="checkbox" />
+              <p>{todoDetail.title}</p>
+            </div>
+            <button onClick={() => navigate("/todo")}>Back</button>
+          </S.TodoDetailHeader>
+          <S.TodoDetailDate>
+            <p>
+              <span>Created At:</span> {dateFormating(todoDetail.createdAt)}
+            </p>
+            <p>
+              <span>Updated At:</span> {dateFormating(todoDetail.updatedAt)}
+            </p>
+          </S.TodoDetailDate>
+          <S.TodoDetailContent>
+            <p className="content-title">Content</p>
+            <p className="content-body">{todoDetail.content}</p>
+          </S.TodoDetailContent>
+          <S.TodoDetailButtons>
+            <Link to="/todo/edit" state={{ id }}>
+              <Button content="Edit" />
+            </Link>
+            <Button content="Delete" onClick={() => deleteAction(todoDetail.id)} />
+          </S.TodoDetailButtons>
+        </>
+      )}
     </S.ComponentWrapper>
   );
 }
